e38b5b63fedac50cebdeeb7aea6a7f4ae74574f8
Luan Ramos <luan.developer@gmail.com>
2023-11-15 16:09:49 -0300

    comments about difference into define and const

--- basics_contants_demonstration.php
+++ basics_contants_demonstration.php
@@ -1,6 +1,9 @@
 <?php 
 
+// if you are using a namespace, the define method you generate a warning
 define("UNITS", ['MILES_CONVERSION' => 1.6, "INCHES_CONVERSION" => '2.14']); // using define method
+
+// if you have a namespace, you should use const keyword. That is the difference
 const NUMBER_OF_KM = 21.87654; // using keyword const
 
 echo NUMBER_OF_KM." km(s) in miles is " . NUMBER_OF_KM * UNITS['MILES_CONVERSION']."\n";
