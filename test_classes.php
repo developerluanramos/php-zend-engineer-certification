0e0527c6aa502a76fe471098fdae3609de8dd6c6
Luan Ramos <luan.developer@gmail.com>
2023-11-15 00:32:54 -0300

    testing classes

---
+++ test_classes.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Model {
+
+    public function __construct() { }
+
+    static function all() {}
+
+    public function where() {}
+
+    public function get() {}
+}
+
+class User extends Model {
+    public function __construct() {}
+}
+
