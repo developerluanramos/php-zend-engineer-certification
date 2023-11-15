26c0032177d62c00eb7b124888f2671f8f74e9a5
Luan Ramos <luan.developer@gmail.com>
2023-11-15 16:06:15 -0300

    comments about constants

--- basics_contants_demonstration.php
+++ basics_contants_demonstration.php
@@ -1,6 +1,6 @@
 <?php 
 
-define("UNITS", ['MILES_CONVERSION' => 1.6, "INCHES_CONVERSION" => '2.14']);
-const NUMBER_OF_KM = 21.87654;
+define("UNITS", ['MILES_CONVERSION' => 1.6, "INCHES_CONVERSION" => '2.14']); // using define method
+const NUMBER_OF_KM = 21.87654; // using keyword const
 
 echo NUMBER_OF_KM." km(s) in miles is " . NUMBER_OF_KM * UNITS['MILES_CONVERSION']."\n";
