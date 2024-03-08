ce19a47e3cdb0b1d2d21cb1d42c71a6b48940fce
Luan Ramos <luan.developer@gmail.com>
2024-03-08 00:58:05 -0300

    basics language constructs

---
+++ 2-basics_language_constructs.php
@@ -0,0 +1,8 @@
+<?php 
+
+// -- its different from functions because function returns a value ever, or null.
+// -- language constructs not do this. LCs are
+
+echo "Hello \r\n";
+echo ("teste"); // it not works with 2 params
+echo "Hello",  "Word"; // works with 2 params
--- basics_language_constructs.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-// -- its different from functions
-echo "Hello \r\n";
-echo ("teste"); // it not works with 2 params
-echo "Hello",  "Word"; // works with 2 params
