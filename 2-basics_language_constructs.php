b2234ce5de16867018aea93bfd7cdea3827a15da
Luan Ramos <luan.developer@gmail.com>
2024-03-08 01:00:40 -0300

    basics language constructs

--- 2-basics_language_constructs.php
+++ 2-basics_language_constructs.php
@@ -3,6 +3,25 @@
 // -- its different from functions because function returns a value ever, or null.
 // -- language constructs not do this. LCs are
 
+/*
+ * assert
+ * echo
+ * print
+ * exit
+ * die
+ * return
+ * include
+ * include_once
+ * require
+ * require_once
+ * eval
+ * empty
+ * isset
+ * unset
+ * list
+ *
+ * */
+
 echo "Hello \r\n";
 echo ("teste"); // it not works with 2 params
 echo "Hello",  "Word"; // works with 2 params
