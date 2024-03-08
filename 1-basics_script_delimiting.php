e207b587201b237f7c7b83aaf4028994544c29e5
Luan Ramos <luan.developer@gmail.com>
2024-03-08 00:56:57 -0300

    basics script delimiting

---
+++ 1-basics_script_delimiting.php
@@ -0,0 +1,10 @@
+<!--
+there are some ways to delimit the PHP code.
+
+<?php echo "foo"; // standard mode ?>
+<? echo "foo"; // deprecated ?>
+<?= 'foo' // echo mode used to display something without echo keyword ?>
+<script language="php"> echo 'foo' </script> // -> do note use this
+<% %> // ASP mode deprecated
+-->
+
--- basics_script_delimiting.php
+++
@@ -1,2 +0,0 @@
-<?php // standard mode ?>
-<?= 'test echo without echo keywork' // echo mode used to display something without echo keyword ?>
